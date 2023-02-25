92fafa1ea3beee7dbc8cbf054c425a267c510eb6
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-02-25 11:25:32 +0800

    perf: output JSON

--- api/document.php
+++ api/document.php
@@ -7,16 +7,27 @@ require_once __DIR__ . '/utils.php';
 cors();
 
 $upload = !empty($_GET['upload']) ? $conn->real_escape_string($_GET['upload']) : null;
-if ($upload === null) die('Invalid params');
+if ($upload === null) {
+  die(json_encode([
+    'success' => false,
+    'message' => 'Invalid params'
+  ]));
+}
 
 $receipt_res = $conn->query("SELECT * FROM documents WHERE upload='$upload' LIMIT 1");
-if ($receipt_res->num_rows === 0) die('Invalid params');
+if ($receipt_res->num_rows === 0) {
+  die(json_encode([
+    'success' => false,
+    'message' => 'Document was not found'
+  ]));
+}
 
 $receipt = $receipt_res->fetch_object();
 $upload_path = __DIR__ . "/data/$upload.pdf";
 
 header('Content-Type: application/json');
 echo json_encode([
+  'success' => true,
   'idnumber' => $receipt->idnumber,
   'filename' => $receipt->filename,
   'copies' => intval($receipt->copies),
