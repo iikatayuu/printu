560bf774976467fb0f1f03277fa6d4930b15c042
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-03-23 22:59:18 +0800

    fix: add multiple support for payments

--- success.php
+++ success.php
@@ -14,13 +14,16 @@ if ($id === null) die('Invalid params');
 $receipt_res = $conn->query("SELECT * FROM documents WHERE id=$id LIMIT 1");
 if ($receipt_res->num_rows === 0) die('Invalid params');
 
+$token = base64_encode("$xendit_secret_key:");
 $receipt = $receipt_res->fetch_object();
 $email = $receipt->email;
 $payment = $receipt->payment;
 $upload = $receipt->upload;
-$token = base64_encode("$xendit_secret_key:");
+$payments = explode('|', $payment);
+
+$gcash = $payments[0];
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://api.xendit.co/payment_requests/$payment");
+curl_setopt($curl, CURLOPT_URL, "https://api.xendit.co/payment_requests/$gcash");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . '/api/cacert.pem');
 curl_setopt($curl, CURLOPT_HTTPHEADER, [
@@ -32,8 +35,27 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, [
 $output = curl_exec($curl);
 curl_close($curl);
 
-$json = json_decode($output);
-if ($json->status === 'SUCCEEDED') {
+echo $output . PHP_EOL;
+$json1 = json_decode($output);
+
+$paymaya = $payments[1];
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://api.xendit.co/payment_requests/$paymaya");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . '/api/cacert.pem');
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+  'Accept: application/json',
+  'Content-Type: application/json',
+  "Authorization: Basic $token"
+]);
+
+$output = curl_exec($curl);
+curl_close($curl);
+
+echo $output . PHP_EOL;
+$json2 = json_decode($output);
+
+if ($json1->status === 'SUCCEEDED' || $json2->status === 'SUCCEEDED') {
   $conn->query("UPDATE documents SET paid=1 WHERE id=$id");
 
   $mail = new PHPMailer(true);
